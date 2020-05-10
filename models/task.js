b9b1e90889a2a63fd5d54b004e761378d3693003
Juan Sanchez <juan.sanchez@ultertechnologies.com>
2020-05-10 05:25:36 -0500

    [feat]: refactor task model and change the environement for test

--- models/task.js
+++ models/task.js
@@ -6,8 +6,6 @@ const schema = new mongoose.Schema({
     name: { type: String },
     user: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
     state: { type: String, required: true, default: constants.TASK_STATE.IN_PROGRESS, enum: [constants.TASK_STATE.IN_PROGRESS, constants.TASK_STATE.PAUSED, constants.TASK_STATE.RESTARTED] },
-    startDateTime: { type: Date, default: new Date() },
-    endDateTime: Date,
     duration: { type: Number, default: 0 },
 }, {
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
